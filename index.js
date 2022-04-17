3517406ac2ac84fc015425060c7aed434dbcdcd6
vidhi31 <91669015+vidhi31@users.noreply.github.com>
2022-04-17 09:35:06 +0530

    js modified

--- index.js
+++ index.js
@@ -1,30 +1,23 @@
-const tabItems = document.querySelectorAll('.tab-item');  
-const tabContentItems = document.querySelectorAll('.tab-content-item');  
-// Select tab content item  
-function selectItem(e) {  
-     // Remove all show and border classes  
+var tabItems = document.querySelectorAll('.tab-item');  
+var tabContentItems = document.querySelectorAll('.tab-content-item');  
+  
+function selectItem(e) {    
      removeBorder();  
-     removeShow();  
-     // Add border to current tab item  
-     this.classList.add('tab-border');  
-     // Grab content item from DOM  
-     const tabContentItem = document.querySelector(`#${this.id}-content`);  
-     // Add show class  
+     removeShow();    
+     this.classList.add('tab-border');   
+     var tabContentItem = document.querySelector(`#${this.id}-content`);   
      tabContentItem.classList.add('show');  
-}  
-// Remove bottom borders from all tab items  
+}    
 function removeBorder() {  
      tabItems.forEach(item => {  
           item.classList.remove('tab-border');  
      });  
-}  
-// Remove show class from all content items  
+}    
 function removeShow() {  
      tabContentItems.forEach(item => {  
           item.classList.remove('show');  
      });  
-}  
-// Listen for tab item click  
+}    
 tabItems.forEach(item => {  
      item.addEventListener('click', selectItem);  
 });
